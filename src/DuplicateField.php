aac3b98a13ee305b048ab8c55328016aa530d4d8
Mantas Brazdauskis <mantas.brazdauskis@oc.eu>
2021-10-19 16:58:00 +0300

    lets fix php8 bug

--- src/DuplicateField.php
+++ src/DuplicateField.php
@@ -14,7 +14,7 @@ class DuplicateField extends Field
      */
     public $component = 'duplicate-field';
 
-    public function __construct(string $name, ? string $attribute = null, ? mixed $resolveCallback = null)
+    public function __construct(string $name, string $attribute = null, $resolveCallback = null)
     {
         parent::__construct(null, null, null);
 
